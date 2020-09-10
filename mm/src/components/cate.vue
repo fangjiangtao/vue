a2c70e6f4c79ef7a88233e93ca84a01d2255f435
username <email>
2020-09-10 10:45:26 +0800

    闭包

---
+++ mm/src/components/cate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      1234
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style>
+</style>
